66da5606e5ceffff055ace6b76f750b562cb9b06
GangPao <127927330+NavyCheng@users.noreply.github.com>
2023-08-29 16:08:38 +0800

    重构代码，并添加了边界判定
    超出边界则自动回到对面

--- game.py
+++ game.py
@@ -1,6 +1,4 @@
 import sys
-import time
-
 import pygame
 from setting import Settings
 from snakepart import SnakePart
@@ -18,16 +16,13 @@ class Game:
         """导入蛇的部分类，对于蛇头和蛇身的设置可以放在这里，这里因为需要导入屏幕，所以要放在self.screen的后面"""
         self.snakepart = SnakePart(self)
 
-        """设定初始方向，具体可以在设置类里面改"""
-        self.direction = self.settings.direction
-
-        self.double_spped = False
-
         """导入屏幕帧数函数"""
         self.clock = pygame.time.Clock()
+
     def run_game(self):
         while True:
             self.check_event()
+            self.snakepart.move_snake()
             self.update_screen()
 
 
@@ -36,23 +31,23 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
 
-                if event.key == pygame.K_RIGHT and self.direction != "LEFT":
-                    self.direction = "RIGHT"
-                elif event.key == pygame.K_UP and self.direction != "DOWN":
-                    self.direction = "UP"
-                elif event.key == pygame.K_LEFT and self.direction != "RIGHT":
-                    self.direction = "LEFT"
-                elif event.key == pygame.K_DOWN and self.direction != "UP":
-                    self.direction = "DOWN"
+                """检测按键，上下左右，空格加速"""
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT and self.snakepart.direction != "LEFT":
+                    self.snakepart.direction = "RIGHT"
+                elif event.key == pygame.K_UP and self.snakepart.direction != "DOWN":
+                    self.snakepart.direction = "UP"
+                elif event.key == pygame.K_LEFT and self.snakepart.direction != "RIGHT":
+                    self.snakepart.direction = "LEFT"
+                elif event.key == pygame.K_DOWN and self.snakepart.direction != "UP":
+                    self.snakepart.direction = "DOWN"
                 elif event.key == pygame.K_SPACE:
-                    self.double_spped = True
+                    self.snakepart.double_spped = True
 
             elif event.type == pygame.KEYUP:
-
                 if event.key == pygame.K_SPACE:
-                    self.double_spped = False
+                    self.snakepart.double_spped = False
 
 
     def update_screen(self):
@@ -62,36 +57,10 @@ class Game:
         self.screen.fill(self.settings.bg_color)
         """绘制蛇头"""
         self.snakepart.blitsnakehead()
-        """绘制蛇的移动"""
-        self.move_snake()
         """设定帧率"""
         self.clock.tick(30)
-
         """刷新屏幕"""
         pygame.display.flip()
-
-    def move_snake(self):
-        """定义一个控制蛇移动方向的函数"""
-        if self.direction == "RIGHT":
-            if self.double_spped == True:
-                self.snakepart.rect.x += self.settings.speed * self.settings.speed_multiple
-            elif self.double_spped == False:
-                self.snakepart.rect.x += self.settings.speed
-        elif self.direction == "LEFT":
-            if self.double_spped == True:
-                self.snakepart.rect.x -= self.settings.speed * self.settings.speed_multiple
-            elif self.double_spped == False:
-                self.snakepart.rect.x -= self.settings.speed
-        elif self.direction == "DOWN":
-            if self.double_spped == True:
-                self.snakepart.rect.y += self.settings.speed * self.settings.speed_multiple
-            elif self.double_spped == False:
-                self.snakepart.rect.y += self.settings.speed
-        elif self.direction == "UP":
-            if self.double_spped == True:
-                self.snakepart.rect.y -= self.settings.speed * self.settings.speed_multiple
-            elif self.double_spped == False:
-                self.snakepart.rect.y -= self.settings.speed
 
 
 
